a2716a4a80eb76a4bcc413eb246e8d08f5e69845
maiikol1192 <maiikol1192@gmail.com>
2025-08-07 21:57:36 -0500

    add init page contruction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,74 @@
+"use client";
+
+import Image from "next/image";
+import LogoImage from "@/app/assets/images/logo.webp";
+import CoverImage from "@/app/assets/images/cover.webp";
+
 const Home = () => {
+  const redirectToFacebook = () => {
+    const isMobile =
+      /android|iphone|ipad|ipod|opera mini|iemobile|mobile/i.test(
+        navigator.userAgent,
+      );
+    if (isMobile) {
+      window.location.href = "fb://page/100064789524483";
+      setTimeout(() => {
+        window.open(
+          "https://www.facebook.com/ligadefutbolmeta",
+          "_blank",
+          "noopener,noreferrer",
+        );
+      }, 500);
+    } else {
+      window.open(
+        "https://www.facebook.com/ligadefutbolmeta",
+        "_blank",
+        "noopener,noreferrer",
+      );
+    }
+  };
+
   return (
-    <div className="h-dvh flex flex-col gap-5 items-center justify-center bg-orange-50 p-5">
-      <h1 className="text-4xl font-bold text-secondary text-center">
-        Liga de futbol del Meta
-      </h1>
-      <p className="text-center text-main">
-        © Todos los derechos reservados {new Date().getFullYear()}
-      </p>
+    <div className="flex flex-col p-5 sm:p-10 bg-emerald-50 h-dvh overflow-y-auto">
+      <div className="bg-white rounded-2xl w-full grid grid-cols-3 shadow-xl shadow-emerald-500/20 flex-1">
+        <Image
+          src={CoverImage}
+          alt="Logo"
+          className="h-full w-full object-cover object-center rounded-l-2xl hidden sm:block"
+        />
+        <div className="col-span-3 sm:col-span-2 flex flex-col gap-5 items-center justify-center p-5 text-center max-w-2xl m-auto">
+          <Image
+            src={LogoImage}
+            alt="Logo"
+            className="w-full max-w-[14rem] h-full object-cover object-center"
+          />
+          <div className="flex flex-col gap-2">
+            <h1 className="text-4xl sm:text-5xl font-bold text-emerald-800">
+              Página en construcción
+            </h1>
+            <p className="text-xl sm:text-2xl text-main">
+              Oops! Estás pisando terreno fuera de juego ⚽
+            </p>
+          </div>
+          <p className="text-sm sm:text-base font-light">
+            Pero no te preocupes—muy pronto estaremos listos para el gran
+            partido. Mientras tanto, síguenos en Facebook y mantente al tanto de
+            todas las novedades de la liga.
+          </p>
+          <button
+            onClick={redirectToFacebook}
+            className="bg-emerald-800 text-white px-6 py-3 rounded-lg hover:bg-emerald-900 transition-colors duration-300 cursor-pointer text-sm sm:text-base"
+          >
+            Visítanos en Facebook
+          </button>
+        </div>
+      </div>
+      <footer className="px-6 sm:px-10 pt-10 flex items-center justify-between text-center mx-auto">
+        <p className="text-xs text-emerald-900/70">
+          © {new Date().getFullYear()} Liga de Fútbol del Meta. Todos los
+          derechos reservados. Hecho con pasión por el fútbol.
+        </p>
+      </footer>
     </div>
   );
 };
